bb17877a5f4cdaacffff0ad4ed4fcb190bf30554
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-12-20 13:17:08 +0530

    add category feature in expense model

--- lib/services/Models/expense.dart
+++ lib/services/Models/expense.dart
@@ -1,16 +1,25 @@
 import 'package:uuid/uuid.dart';
 
-const uuid = Uuid();  
+const uuid = Uuid();
+
+enum Category{
+  food,
+  travel,
+  leisure,
+  work
+}
 
 class Expense {
   Expense({
     required this.title, 
     required this.amount, 
-    required this.date
-  }) : id = uuid.v4();    // Used to generate unique id on each object creation by using uuid package
+    required this.date,
+    required this.category
+  }): id = uuid.v4(); // Used to generate unique id on each object creation by using uuid package
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
