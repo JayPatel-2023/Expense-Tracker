1bf0b93698ba9ad0ddb6a8568e060c1457db3a59
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-12-21 12:52:26 +0530

    new expense complete

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -52,14 +52,13 @@ class _NewExpenseState extends State<NewExpense> {
       return;
     }
 
-    widget.onAddExpense(
-      Expense(
-        title: _titleController.text, 
-        amount: enteredAmount, 
-        date: _selectedDate!, 
-        category: _selectedCategory
-      )
-    );
+    widget.onAddExpense(Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory));
+
+    Navigator.pop(context);
   }
 
   @override
