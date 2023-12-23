82c95295b9a2c5a8432ab5d9847441712efc5271
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-12-23 19:51:22 +0530

    locking the device orientiation

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:expense_tracker/Pages/expenses.dart';
 import 'package:expense_tracker/utils/colors.dart';
 import 'package:expense_tracker/utils/styles.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  WidgetsFlutterBinding.ensureInitialized();    // orientation locking
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((value) {
+    // The app only starts when the device orientation = portraitup
+    runApp(MaterialApp(
       darkTheme: ThemeData.dark(
         useMaterial3: true,
-        ).copyWith(
-          colorScheme: kDarkColorScheme,
-          appBarTheme: darkAppbarTheme,
-          cardTheme: darkCardTheme,
-          elevatedButtonTheme: darkButtonTheme,
-          textTheme: darkTxtTheme,
+      ).copyWith(
+        colorScheme: kDarkColorScheme,
+        appBarTheme: darkAppbarTheme,
+        cardTheme: darkCardTheme,
+        elevatedButtonTheme: darkButtonTheme,
+        textTheme: darkTxtTheme,
       ),
-      theme: ThemeData(
-        useMaterial3: true
-        ).copyWith(
-          colorScheme: kColorScheme,
-          appBarTheme: appbarTheme,
-          cardTheme: cardTheme,
-          elevatedButtonTheme: buttonTheme,
-          textTheme: txtTheme,
+      theme: ThemeData(useMaterial3: true).copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: appbarTheme,
+        cardTheme: cardTheme,
+        elevatedButtonTheme: buttonTheme,
+        textTheme: txtTheme,
       ),
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
-        home: const Expenses(),
-      )
-    );
+      home: const Expenses(),
+    ));
+  });
 }
-
